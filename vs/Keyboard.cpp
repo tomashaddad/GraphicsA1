c06c0b2a8411b043da3917716d3906ed0126fb34
Tomas Haddad <tomashaddad@gmail.com>
2021-04-16 20:28:05 +1000

    filters and case fix

--- vs/Keyboard.cpp
+++ vs/Keyboard.cpp
@@ -1,23 +1,24 @@
 #include "Keyboard.h"
-#include <iostream>
+#include <cctype>
 
 Keyboard::Keyboard()
 	: key_states{ false } {}
 
 void Keyboard::setKeyState(unsigned char key, bool state) {
+	key = std::toupper(key);
 	if (0 <= key && key < 256) {
 		key_states[key] = state;
 	}
 }
 
 bool Keyboard::getKeyState(unsigned char key) {
+	key = std::toupper(key);
 	return key_states[key];
 }
 
 bool Keyboard::anyKeyIsPressed() {
 	for (auto i = 0; i < key_states.size(); ++i) {
 		if (key_states[i]) {
-			std::cout << i << std::endl;
 			return true;
 		}
 	}
